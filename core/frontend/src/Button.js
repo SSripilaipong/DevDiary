40cabdf2afefe23aa210a698e6743d05f721535e
SSripilaipong <santhapon.s@siametrics.com>
2022-02-12 23:37:30 +0700

    init react; copy-paste from https://medium.com/age-of-awareness/setup-react-with-webpack-and-babel-5114a14a47e9 and https://medium.com/swlh/deploying-micro-frontends-to-aws-step-by-step-using-react-webpack-5-and-module-federation-3f97f6f1658c

---
+++ core/frontend/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => (
+  <button>Super Cool Button</button>
+);
+
+export default Button;
---
+++ core/frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path:path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'core',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Button': './src/Button',
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+}
