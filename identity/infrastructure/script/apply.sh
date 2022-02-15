e941635ce45e46f2d62b49f3589530a66a7967ec
SSripilaipong <santhapon.s@siametrics.com>
2022-02-16 00:28:23 +0700

    [identity] fix terraform apply script

--- identity/infrastructure/script/apply.sh
+++ identity/infrastructure/script/apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "${AWS_REGION}" ] || [ -z "${TF_BACKEND_BUCKET_NAME}" ] || [ -z "${TF_BACKEND_BUCKET_PATH}" ]; then
-  echo ERROR: variables needed to be set: AWS_REGION, TF_BACKEND_BUCKET_NAME, TF_BACKEND_BUCKET_PATH 1>&2
+  echo ERROR: variables needed to be set: AWS_REGION \(\"$AWS_REGION\"\), TF_BACKEND_BUCKET_NAME \(\"$TF_BACKEND_BUCKET_NAME\"\), TF_BACKEND_BUCKET_PATH \(\"$TF_BACKEND_BUCKET_PATH\"\) 1>&2
   exit 1
 fi
 
