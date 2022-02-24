ef275827f6dd6a7cc4f9f5100997a4075a062a87
SSripilaipong <santhapon.s@siametrics.com>
2022-02-24 17:36:59 +0700

    feat(core.infrastructure): add PublicRead policy for frontend s3 bucket
    Refs: #1

--- core/infrastructure/src/frontend.tf
+++ core/infrastructure/src/frontend.tf
@@ -9,3 +9,20 @@ resource "aws_s3_bucket" "frontend" {
 resource "aws_s3_bucket_acl" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 }
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Sid       = "PublicRead"
+        Action    = "s3:GetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Resource  = "arn:aws:s3:::devdiary.link/*"
+      },
+    ]
+    Version   = "2012-10-17"
+  }
+  )
+}
