af349fd7793d412d991189da4bc17e5953d3e222
SSripilaipong <santhapon.s@siametrics.com>
2022-02-24 22:24:11 +0700

    experiment(core.frontend): clean up
    Refs: #1

--- core/frontend/src/Pages/Home.js
+++ core/frontend/src/Pages/Home.js
@@ -1,11 +1,7 @@
 import React from 'react';
-const IdentityButton = React.lazy(() => import('identity/Button'));
 
 export default function HomePage() {
     return (<div>
         <div>This is Home Page</div>
-        <React.Suspense fallback='Loading Button'>
-            <IdentityButton/>
-        </React.Suspense>
     </div>);
 }
