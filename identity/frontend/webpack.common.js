5f6218cd201b6a5ffb8ae7c282139e4420346262
SSripilaipong <santhapon.s@siametrics.com>
2022-02-25 17:14:23 +0700

    fix(identity.frontend): shared all dependencies
    Refs: #1

--- identity/frontend/webpack.common.js
+++ identity/frontend/webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require('./package.json').dependencies;
 
 module.exports = {
     entry: path.join(__dirname, "src", "index.js"),
@@ -27,6 +29,24 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
+        }),
+        new ModuleFederationPlugin({
+            name: 'identity',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Register': './src/Pages/Register',
+            },
+            shared: {
+                ...deps,
+                react: {
+                    singleton: true,
+                    requiredVersion: deps.react,
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: deps['react-dom'],
+                },
+            },
         }),
     ],
 }
--- identity/frontend/webpack.prod.js
+++ identity/frontend/webpack.prod.js
@@ -1,22 +1,6 @@
 const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: "production",
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'identity',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './Register': './src/Pages/Register',
-            },
-            shared: {
-                react: {
-                    eager: true,
-                    singleton: true,
-                },
-            },
-        }),
-    ],
 })
