d1438e73840cc43767b8911545ab9c9b41b536a1
SSripilaipong <santhapon.s@siametrics.com>
2022-02-28 00:05:28 +0700

    fix(core.infrastructure): resolve cycle
    Refs: #1

--- identity/infrastructure/src/backend.tf
+++ identity/infrastructure/src/backend.tf
@@ -1,24 +1,9 @@
-resource "aws_lambda_function" "backend" {
-  function_name = "${var.GLOBAL_PREFIX}-identity-backend"
-
-  s3_bucket = data.aws_s3_bucket.resource-sharing.id
-  s3_key    = "template/lambda-default.zip"
-
-  runtime = "python3.9"
-  handler = "default.handler"
-
-  source_code_hash = base64sha256("NO NEED")
-
-  role = aws_iam_role.backend-exec.arn
-}
-
-resource "aws_cloudwatch_log_group" "backend" {
-  name = "/aws/lambda/${aws_lambda_function.backend.function_name}"
-  retention_in_days = 1
+locals {
+  BACKEND_NAME = "${var.GLOBAL_PREFIX}-identity-backend"
 }
 
 resource "aws_iam_role" "backend-exec" {
-  name = aws_lambda_function.backend.function_name
+  name = local.BACKEND_NAME
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -33,7 +18,32 @@ resource "aws_iam_role" "backend-exec" {
   })
 }
 
+resource "aws_lambda_function" "backend" {
+  function_name = local.BACKEND_NAME
+
+  s3_bucket = data.aws_s3_bucket.resource-sharing.id
+  s3_key    = "template/lambda-default.zip"
+
+  runtime = "python3.9"
+  handler = "default.handler"
+
+  source_code_hash = base64sha256("NO NEED")
+
+  role = aws_iam_role.backend-exec.arn
+  depends_on = [
+    aws_iam_role.backend-exec,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "backend" {
+  name = "/aws/lambda/${local.BACKEND_NAME}"
+  retention_in_days = 1
+}
+
 resource "aws_iam_role_policy_attachment" "backend-policy" {
   role       = aws_iam_role.backend-exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  depends_on = [
+    aws_iam_role.backend-exec,
+  ]
 }
