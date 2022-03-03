dda1c1afb5f3dbc96fcc673aa2af23833b184260
SSripilaipong <santhapon.s@siametrics.com>
2022-03-03 14:46:30 +0700

    fix(identity.backend): typo
    Refs: #1

--- identity/backend/src/main.py
+++ identity/backend/src/main.py
@@ -5,7 +5,7 @@ from lambda_event.api_gateway import RawApiGatewayEvent
 def handler(event, context):
     print('context:', context)
     print('event:', event)
-    print(RawApiGatewayEvent(event))
+    print(RawApiGatewayEvent(**event))
     return {
         "statusCode": 200,
         "headers": {
