39c4334709ca551ffb984cdf0a25ddd879fdfb3d
SSripilaipong <santhapon.s@siametrics.com>
2022-03-03 16:30:35 +0700

    feat(identity.infrastructure): add register endpoint
    Refs: #1

---
+++ identity/infrastructure/src/backend-endpoint.tf
@@ -0,0 +1,19 @@
+resource "aws_apigatewayv2_integration" "register" {
+  api_id = data.aws_apigatewayv2_api.core.id
+
+  integration_uri    = aws_lambda_function.backend.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  payload_format_version = "2.0"
+
+  request_parameters = {
+    "overwrite:header.X-${var.APP_NAME}-Operation": "IdentityRegister",
+  }
+}
+
+resource "aws_apigatewayv2_route" "register" {
+  api_id = data.aws_apigatewayv2_api.core.id
+
+  route_key = "POST /users/register"
+  target    = "integrations/${aws_apigatewayv2_integration.register.id}"
+}
