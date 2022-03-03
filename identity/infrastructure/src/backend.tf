27a279474549510d5d6b9936b851708c81c127a7
SSripilaipong <santhapon.s@siametrics.com>
2022-03-03 15:56:40 +0700

    experiment(identity.infrastructure): try add headers to lambda
    Refs: #1

--- identity/infrastructure/src/backend.tf
+++ identity/infrastructure/src/backend.tf
@@ -82,7 +82,7 @@ resource "aws_apigatewayv2_integration" "say-hello" {
   payload_format_version = "2.0"
 
   request_parameters = {
-    "integration.request.header.X-DevDiary-Operation": "'SayHello'",
+    "integration.headers.X-DevDiary-Operation": "'SayHello'",
   }
 }
 
@@ -102,7 +102,7 @@ resource "aws_apigatewayv2_integration" "say-hello-with-body" {
   payload_format_version = "2.0"
 
   request_parameters = {
-    "integration.request.header.X-DevDiary-Operation": "'SayHelloWithBody'",
+    "integration.headers.X-DevDiary-Operation": "'SayHelloWithBody'",
   }
 }
 
