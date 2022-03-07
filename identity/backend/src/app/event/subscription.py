459d7f187b738879a388ffe4cd0298b69d541804
SSripilaipong <santhapon.s@siametrics.com>
2022-03-07 23:30:49 +0700

    experiment(identity.backend): turn subscriptions back on
    Refs: #1

--- identity/backend/src/app/event/subscription.py
+++ identity/backend/src/app/event/subscription.py
@@ -5,6 +5,5 @@ from domain.identity.registration.event.confirmed import RegistrationConfirmedEv
 
 
 def subscribe_for_messages(message_bus: MessageBus):
-    # message_bus.subscribe(RegistrationEmailNeededToBeConfirmedEvent, handle_email_needed_confirmation)
-    # message_bus.subscribe(RegistrationConfirmedEvent, handle_registration_confirmed)
-    pass
+    message_bus.subscribe(RegistrationEmailNeededToBeConfirmedEvent, handle_email_needed_confirmation)
+    message_bus.subscribe(RegistrationConfirmedEvent, handle_registration_confirmed)
