988621f5dc5b1c991a29729c0dbecda7e5aa7e87
SSripilaipong <santhapon.s@siametrics.com>
2022-03-11 01:05:59 +0700

    feat(identity.frontend): export DefaultRegisterPage to core frontend
    Refs: #1

--- core/frontend/src/Pages/Register.js
+++ core/frontend/src/Pages/Register.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const RemoteRegisterPage = React.lazy(() => import('identity/Register')
-    .then(module => ({default: module.RegisterPage})));
+    .then(module => ({default: module.DefaultRegisterPage})));
 
 export default function RegisterPage() {
     return (<React.Suspense fallback='Loading'>
--- identity/frontend/src/Pages/Register/index.tsx
+++ identity/frontend/src/Pages/Register/index.tsx
@@ -1,1 +1,12 @@
+import DummyRegistrationService from "../../Service/Registration/Dummy";
+import React from "react";
+import {RegisterPageController} from "./Controller";
+
 export { RegisterPageController as RegisterPage } from "./Controller";
+
+export const DefaultRegisterPage = () => {
+    return <RegisterPageController
+        redirectUrlPath={"/"}
+        registrationService={new DummyRegistrationService()}
+    />;
+}
