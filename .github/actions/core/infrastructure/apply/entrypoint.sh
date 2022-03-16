dc19960eb6885998df53295a7ed3e46ef6c67d8d
SSripilaipong <santhapon.s@siametrics.com>
2022-03-16 13:09:27 +0700

    build(core.infrastructure): move template/ to src/
    Refs: #1

--- .github/actions/core/infrastructure/apply/entrypoint.sh
+++ .github/actions/core/infrastructure/apply/entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "${AWS_REGION}" ] || [ -z "${TF_BACKEND_BUCKET_NAME}" ] || [ -z "${TF_BA
   exit 1
 fi
 
-sed s/AWS_REGION/"$AWS_REGION"/g ../template/backend.template.tf |
+sed s/AWS_REGION/"$AWS_REGION"/g ./template/backend.template.tf |
   sed s/TF_BACKEND_BUCKET_NAME/"$TF_BACKEND_BUCKET_NAME"/g |
   sed s:TF_BACKEND_BUCKET_PATH:"$TF_BACKEND_BUCKET_PATH":g > backend.tf
 terraform init
