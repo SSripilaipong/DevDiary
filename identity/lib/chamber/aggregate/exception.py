cce65ad5b851d4af5a443c2966756e0e032ef5e8
SSripilaipong <santhapon.s@siametrics.com>
2022-03-22 23:47:11 +0700

    test(chamber.aggregate.Field): add unit test test_should_raise_FieldHasNoGetterException_when_try_to_access_private_field_from_outside
    Refs: #1

--- identity/lib/chamber/aggregate/exception.py
+++ identity/lib/chamber/aggregate/exception.py
@@ -1,2 +1,6 @@
 class InvalidAggregateVersionException(Exception):
     pass
+
+
+class FieldHasNoGetterException(Exception):
+    pass
--- identity/lib/tests/chamber/test_aggregate/test_field.py
+++ identity/lib/tests/chamber/test_aggregate/test_field.py
@@ -1,5 +1,6 @@
 from pytest import raises
 from chamber.aggregate import Aggregate, Field
+from chamber.aggregate.exception import FieldHasNoGetterException
 
 
 def test_should_instantiate_aggregate_with_fields():
@@ -22,3 +23,13 @@ def test_should_raise_RuntimeError_when_instantiate_without_type():
     with raises(RuntimeError):
         class _(Aggregate):
             my_number = Field()
+
+
+def test_should_raise_FieldHasNoGetterException_when_try_to_access_private_field_from_outside():
+    class MyAggregate(Aggregate):
+        my_number: int = Field()
+
+    obj = MyAggregate(my_number=123)
+
+    with raises(FieldHasNoGetterException):
+        print(obj.my_number)
