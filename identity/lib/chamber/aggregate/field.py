c6c0158181693fee3013ee99018cc318a6d9a951
SSripilaipong <santhapon.s@siametrics.com>
2022-03-22 23:41:55 +0700

    feat(chamber.aggregate.Field): handle AttributeError for unit test test_should_raise_RuntimeError_when_instantiate_without_type()
    Refs: #1

--- identity/lib/chamber/aggregate/field.py
+++ identity/lib/chamber/aggregate/field.py
@@ -2,12 +2,19 @@ from typing import Type
 
 from chamber.aggregate import Aggregate
 
+FIELD_MUST_HAVE_TYPE_MSG = "A field must be annotated with a type."
+
 
 class Field:
     def __set_name__(self, owner: Type[Aggregate], name: str):
-        self._type = owner.__annotations__.get(name, None)
+        try:
+            annotations = owner.__annotations__
+        except AttributeError:
+            raise TypeError(FIELD_MUST_HAVE_TYPE_MSG)
+
+        self._type = annotations.get(name, None)
         if self._type is None:
-            return  # TODO: handle this
+            raise TypeError(FIELD_MUST_HAVE_TYPE_MSG)
 
     def __set__(self, instance: Aggregate, value):
         if not isinstance(value, self._type):
