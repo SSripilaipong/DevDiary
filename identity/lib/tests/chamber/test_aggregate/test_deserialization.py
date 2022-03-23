9a47329cf4b34eac42ab6cde7cf2a05a2d86271a
SSripilaipong <santhapon.s@siametrics.com>
2022-03-23 17:19:22 +0700

    test(chamber.aggregate.Aggregate): add unit test test_should_convert_primitive_to_flat()
    Refs: #4

--- identity/lib/tests/chamber/test_aggregate/test_deserialization.py
+++ identity/lib/tests/chamber/test_aggregate/test_deserialization.py
@@ -1,4 +1,5 @@
 from chamber.aggregate import Aggregate, Field
+from chamber.flat.string import StringFlat
 
 
 def test_should_create_aggregate_from_dict():
@@ -7,3 +8,11 @@ def test_should_create_aggregate_from_dict():
 
     obj = MyAggregate.from_dict({"my_number": 123})
     assert obj.my_number == 123
+
+
+def test_should_convert_primitive_to_flat():
+    class MyAggregate(Aggregate):
+        my_string: StringFlat = Field(getter=True)
+
+    obj = MyAggregate.from_dict({"my_string": "Copy Paste Engineer"})
+    assert obj.my_string == "Copy Paste Engineer"
