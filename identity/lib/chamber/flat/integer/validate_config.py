cf79745555ccd9e03e79bdf8e25cb011aa7492a8
SSripilaipong <santhapon.s@siametrics.com>
2022-03-24 22:18:40 +0700

    feat(chamber.flat.integer): raise TypeError when MIN_VALUE config is not int for unit test test_should_raise_TypeError_when_config_MIN_VALUE_with_float()
    Refs: #6

--- identity/lib/chamber/flat/integer/validate_config.py
+++ identity/lib/chamber/flat/integer/validate_config.py
@@ -1,10 +1,11 @@
 from typing import Union
 
 
-def _validate_min_value_config(min_length) -> Union[int, float]:
-    if min_length is not None:
-        assert isinstance(min_length, int)
-        return min_length
+def _validate_min_value_config(min_value) -> Union[int, float]:
+    if min_value is not None:
+        if not isinstance(min_value, int):
+            raise TypeError(f"MIN_VALUE config should be integer (got {min_value}).")
+        return min_value
     return 0
 
 
