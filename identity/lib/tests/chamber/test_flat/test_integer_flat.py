5c5808a4330d8d338d0ca2b7bd3a9c30e25f719d
SSripilaipong <santhapon.s@siametrics.com>
2022-03-24 22:13:24 +0700

    test(chamber.flat.integer): add completed unit test test_should_allow_config_MIN_VALUE()
    Refs: #6

--- identity/lib/tests/chamber/test_flat/test_integer_flat.py
+++ identity/lib/tests/chamber/test_flat/test_integer_flat.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from chamber.flat.integer import IntegerFlat
 
 
@@ -5,3 +7,13 @@ def test_should_create_IntegerFlat():
     class MyFlat(IntegerFlat):
         pass
     assert MyFlat(123).int() == 123
+
+
+def test_should_allow_config_MIN_VALUE():
+    class MyFlat(IntegerFlat):
+        MIN_VALUE = 5
+
+    assert MyFlat(5).int() == 5
+
+    with raises(MyFlat.TooLowException):
+        MyFlat(4)
