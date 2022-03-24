04a970032479065d32aa46d78019cc1ee547b6fb
SSripilaipong <santhapon.s@siametrics.com>
2022-03-24 17:40:58 +0700

    refactor(identity.backend): use chamber.aggregate.Field for password_hashed
    Refs: #5

--- identity/backend/src/domain/identity/registration/registration.py
+++ identity/backend/src/domain/identity/registration/registration.py
@@ -12,11 +12,11 @@ from domain.identity.value_object.username import Username
 
 class Registration(Aggregate):
     username: Username = Field("username", getter=True)
+    password_hashed: bytes = Field("passwordHashed", getter=True)
 
     def __init__(self, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email, is_confirmed: bool,
                  confirmation_code: str, _version: AggregateVersion):
-        super().__init__(username=username, _aggregate_version=_version)
-        self._password_hashed = password_hashed
+        super().__init__(username=username, password_hashed=password_hashed, _aggregate_version=_version)
         self._display_name = display_name
         self._email = email
         self._is_confirmed = is_confirmed
@@ -49,10 +49,6 @@ class Registration(Aggregate):
     @property
     def display_name(self) -> DisplayName:
         return self._display_name
-
-    @property
-    def password_hashed(self) -> bytes:
-        return self._password_hashed
 
     @property
     def email(self) -> Email:
