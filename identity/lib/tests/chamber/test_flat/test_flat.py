d8528cd4ab4326c5b1c0fd54fc3789bbd8fbe237
SSripilaipong <santhapon.s@siametrics.com>
2022-03-24 22:58:08 +0700

    test(chamber.flat.base): rename test_should_raise_InvalidTypeException_when_type_is_wrong() -> test_should_raise_InvalidTypeException_when_type_is_wrong_and_cast_failed()
    Refs: #6

--- identity/lib/tests/chamber/test_flat/test_flat.py
+++ identity/lib/tests/chamber/test_flat/test_flat.py
@@ -3,6 +3,6 @@ from pytest import raises
 from chamber.flat.base import Flat
 
 
-def test_should_raise_InvalidTypeException_when_type_is_wrong():
+def test_should_raise_InvalidTypeException_when_type_is_wrong_and_cast_failed():
     with raises(Flat.InvalidTypeException):
         Flat("Hello", int)
