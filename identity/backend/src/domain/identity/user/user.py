5c1ccba42c3063efe748f67af89d9a80e36c6b17
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 14:13:52 +0700

    refactor(identity.backend.User): use Field for display_name
    Refs: #8

--- identity/backend/src/domain/identity/user/user.py
+++ identity/backend/src/domain/identity/user/user.py
@@ -9,19 +9,16 @@ from domain.identity.value_object.username import Username
 class User(Aggregate):
     username: Username = Field(getter=True)
     password_hashed: bytes = Field("passwordHashed", getter=True)
+    display_name: DisplayName = Field("displayName", getter=True)
 
     def __init__(self, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email,
                  _version: AggregateVersion):
-        super().__init__(username=username, password_hashed=password_hashed, _aggregate_version=_version)
-        self._display_name = display_name
+        super().__init__(username=username, password_hashed=password_hashed, display_name=display_name,
+                         _aggregate_version=_version)
         self._email = email
 
     @classmethod
     def create(cls, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email) -> 'User':
         user = cls(username, password_hashed, display_name, email, AggregateVersion(0))
-        user._append_message(UserCreatedEvent(user.username, user._display_name, user._email))
+        user._append_message(UserCreatedEvent(user.username, user.display_name, user._email))
         return user
-
-    @property
-    def display_name(self) -> DisplayName:
-        return self._display_name
