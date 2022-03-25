06345460803d2df4d0b8b5e6e84e1fc137c81be5
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 13:56:59 +0700

    test(chamber.aggregate.version_increase): add unit test test_should_raise_InvalidAggregateVersionException_when_create_with_negative_value()
    Refs: #7

--- identity/lib/tests/chamber/test_aggregate/test_version_increase.py
+++ identity/lib/tests/chamber/test_aggregate/test_version_increase.py
@@ -1,6 +1,14 @@
+from pytest import raises
+
+from chamber.aggregate.exception import InvalidAggregateVersionException
 from chamber.aggregate.version_increase import AggregateVersionIncrease
 
 
 def test_should_allow_creating_version_increase_at_least_0():
     AggregateVersionIncrease(0)
     AggregateVersionIncrease(123)
+
+
+def test_should_raise_InvalidAggregateVersionException_when_create_with_negative_value():
+    with raises(InvalidAggregateVersionException):
+        AggregateVersionIncrease(-1)
