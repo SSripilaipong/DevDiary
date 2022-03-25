2eec65af091992cfcc75894a0f9b560a11099550
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 14:07:44 +0700

    refactor(identity.backend.User): use Field for username
    Refs: #8

--- identity/backend/src/domain/identity/user/user.py
+++ identity/backend/src/domain/identity/user/user.py
@@ -1,4 +1,4 @@
-from chamber.aggregate import Aggregate
+from chamber.aggregate import Aggregate, Field
 from chamber.aggregate.version import AggregateVersion
 from domain.identity.user.event.created import UserCreatedEvent
 from domain.identity.value_object.display_name import DisplayName
@@ -7,10 +7,11 @@ from domain.identity.value_object.username import Username
 
 
 class User(Aggregate):
+    username: Username = Field(getter=True)
+
     def __init__(self, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email,
                  _version: AggregateVersion):
-        super().__init__(_aggregate_version=_version)
-        self._username = username
+        super().__init__(username=username, _aggregate_version=_version)
         self._password_hashed = password_hashed
         self._display_name = display_name
         self._email = email
@@ -18,16 +19,12 @@ class User(Aggregate):
     @classmethod
     def create(cls, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email) -> 'User':
         user = cls(username, password_hashed, display_name, email, AggregateVersion(0))
-        user._append_message(UserCreatedEvent(user._username, user._display_name, user._email))
+        user._append_message(UserCreatedEvent(user.username, user._display_name, user._email))
         return user
 
     @property
     def password_hashed(self) -> bytes:
         return self._password_hashed
-
-    @property
-    def username(self) -> Username:
-        return self._username
 
     @property
     def display_name(self) -> DisplayName:
