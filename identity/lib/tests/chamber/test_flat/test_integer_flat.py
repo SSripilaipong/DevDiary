77abd597bd0912bbb4e20b54960f980951869b68
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 13:22:34 +0700

    test(chamber.flat.integer): add completed unit test test_should_support_greater_than_or_equal_operator()
    Refs: #6

--- identity/lib/tests/chamber/test_flat/test_integer_flat.py
+++ identity/lib/tests/chamber/test_flat/test_integer_flat.py
@@ -77,3 +77,8 @@ def test_should_support_less_than_or_equal_operator():
 
 def test_should_support_greater_than_operator():
     assert IntegerFlat(2) > IntegerFlat(-999)
+
+
+def test_should_support_greater_than_or_equal_operator():
+    assert IntegerFlat(2) >= IntegerFlat(-999)
+    assert IntegerFlat(-999) >= IntegerFlat(-999)
