0e97f6ab667109a9b59f0385294ba9ae0459333f
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 19:11:50 +0700

    test(chamber.message.Message): add unit test test_should_deserialize_from_dict()
    Refs: #9

--- identity/lib/tests/chamber/test_message/test_message.py
+++ identity/lib/tests/chamber/test_message/test_message.py
@@ -9,3 +9,12 @@ def test_should_serialize_to_dict_with_name_and_body():
 
     data = MyMessage(my_number=123, my_string='abc').to_dict()
     assert data == {"name": "MyMessage", "body": {"my_number": 123, "my_string": "abc"}}
+
+
+def test_should_deserialize_from_dict():
+    class MyMessage(Message):
+        my_number: int = Field()
+        my_string: str = Field()
+
+    msg = MyMessage.from_dict({"name": "MyMessage", "body": {"my_number": 123, "my_string": "abc"}})
+    assert msg.my_number == 123 and msg.my_string == 'abc'
