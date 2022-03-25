c61ce2a1fe9c36010c334145808f5481e73ca0e8
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 14:44:11 +0700

    feat(chamber.data.model): validate input keys for unit test test_should_raise_AttributeError_when_instantiate_with_unknown_attribute()
    Refs: #9

--- identity/lib/chamber/data/model.py
+++ identity/lib/chamber/data/model.py
@@ -1,3 +1,26 @@
+from typing import Dict, Set
+
+
 class DataModel:
     def __init__(self, **kwargs):
-        pass
+        self.__chamber_assign_fields(kwargs)
+
+    def __chamber_assign_fields(self, data: Dict):
+        provided_keys = set(data)
+        required_keys = self.__chamber_get_keys_from_annotations()
+        _validate_initial_values(provided_keys, required_keys)
+
+    def __chamber_get_keys_from_annotations(self) -> Set[str]:
+        from chamber.aggregate import Field
+
+        return set(name for name in getattr(self, '__annotations__', {}).keys()
+                   if isinstance(vars(self.__class__).get(name, None), Field))
+
+
+def _validate_initial_values(provided_keys, required_keys):
+    exceeded_keys = provided_keys - required_keys
+    lacked_keys = required_keys - provided_keys
+
+    if exceeded_keys != set():
+        message = "Unknown fields named: " + ', '.join(list(exceeded_keys))
+        raise AttributeError(message)
