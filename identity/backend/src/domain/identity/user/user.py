59f13284c5d4abc0553c73256b84e3a1b5441c2b
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 14:11:32 +0700

    refactor(identity.backend.User): use Field for password_hashed
    Refs: #8

--- identity/backend/src/domain/identity/user/user.py
+++ identity/backend/src/domain/identity/user/user.py
@@ -8,11 +8,11 @@ from domain.identity.value_object.username import Username
 
 class User(Aggregate):
     username: Username = Field(getter=True)
+    password_hashed: bytes = Field("passwordHashed", getter=True)
 
     def __init__(self, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email,
                  _version: AggregateVersion):
-        super().__init__(username=username, _aggregate_version=_version)
-        self._password_hashed = password_hashed
+        super().__init__(username=username, password_hashed=password_hashed, _aggregate_version=_version)
         self._display_name = display_name
         self._email = email
 
@@ -21,10 +21,6 @@ class User(Aggregate):
         user = cls(username, password_hashed, display_name, email, AggregateVersion(0))
         user._append_message(UserCreatedEvent(user.username, user._display_name, user._email))
         return user
-
-    @property
-    def password_hashed(self) -> bytes:
-        return self._password_hashed
 
     @property
     def display_name(self) -> DisplayName:
