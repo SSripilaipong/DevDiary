30ec9b7a42d5b01d93599cfaa6e6079ae31ce61d
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 17:19:12 +0700

    test(chamber.aggregate): move unit test related with aggregate and field to test_aggregate
    Refs: #9

--- identity/lib/tests/chamber/test_data/test_field.py
+++ identity/lib/tests/chamber/test_data/test_field.py
@@ -4,44 +4,6 @@ from chamber.aggregate import Aggregate, query, command
 from chamber.aggregate.exception import FieldHasNoGetterException, FieldHasNoSetterException
 
 
-def test_should_instantiate_aggregate_with_fields():
-    class MyAggregate(Aggregate):
-        my_number: int = Field()
-        my_string: str = Field()
-
-    MyAggregate(my_number=123, my_string="Copy Paste Engineer")
-
-
-def test_should_raise_AttributeError_when_instantiate_with_unknown_attribute():
-    class MyAggregate(Aggregate):
-        pass
-
-    with raises(AttributeError):
-        MyAggregate(something=1234)
-
-
-def test_should_raise_AttributeError_when_instantiate_without_required_attribute():
-    class MyAggregate(Aggregate):
-        my_number: int = Field()
-
-    with raises(AttributeError):
-        MyAggregate()
-
-
-def test_should_raise_TypeError_when_instantiate_with_wrong_type():
-    class MyAggregate(Aggregate):
-        my_number: int = Field()
-
-    with raises(TypeError):
-        MyAggregate(my_number="123")
-
-
-def test_should_raise_RuntimeError_when_instantiate_without_type():
-    with raises(RuntimeError):
-        class _(Aggregate):
-            my_number = Field()
-
-
 def test_should_raise_FieldHasNoGetterException_when_try_to_access_private_field_from_outside():
     class MyAggregate(Aggregate):
         my_number: int = Field()
