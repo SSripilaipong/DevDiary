af93b69fed538fc6462acbc8f704fbe31aec5b9e
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 18:11:25 +0700

    feat(chamber.data.access): make @command support DataModel for unit test test_should_be_able_to_set_value_for_field_without_setter_from_command_method()
    Refs: #9

--- identity/lib/chamber/data/access/command.py
+++ identity/lib/chamber/data/access/command.py
@@ -1,14 +1,19 @@
-from typing import TypeVar
+from typing import TypeVar, Type
 
 from chamber.aggregate import Aggregate
 
 
 class _CommandMethod:
-    def __init__(self, instance: Aggregate, func):
+    def __init__(self, instance: Aggregate, owner: Type, func):
         self._instance = instance
+        self._owner = owner
         self._func = func
 
     def __call__(self, *args, **kwargs):
+        from chamber.data.model import DataModel
+        if issubclass(self._owner, DataModel):
+            with self._instance._DataModel__chamber_request_read_write_access():
+                return self._func(self._instance, *args, **kwargs)
         with self._instance._Aggregate__chamber_field_controller.allow_read_write():
             return self._func(self._instance, *args, **kwargs)
 
@@ -19,7 +24,7 @@ class _Command:
 
     def __get__(self, instance, owner):
         print(instance)
-        return _CommandMethod(instance, self._func)
+        return _CommandMethod(instance, owner, self._func)
 
 
 T = TypeVar("T")
