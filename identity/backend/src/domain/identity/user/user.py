df746060847ab0d3b45474261932cd835bcaccd9
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 14:16:18 +0700

    refactor(identity.backend.User): use Field for email
    Refs: #8

--- identity/backend/src/domain/identity/user/user.py
+++ identity/backend/src/domain/identity/user/user.py
@@ -10,15 +10,11 @@ class User(Aggregate):
     username: Username = Field(getter=True)
     password_hashed: bytes = Field("passwordHashed", getter=True)
     display_name: DisplayName = Field("displayName", getter=True)
-
-    def __init__(self, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email,
-                 _version: AggregateVersion):
-        super().__init__(username=username, password_hashed=password_hashed, display_name=display_name,
-                         _aggregate_version=_version)
-        self._email = email
+    email: Email = Field()
 
     @classmethod
     def create(cls, username: Username, password_hashed: bytes, display_name: DisplayName, email: Email) -> 'User':
-        user = cls(username, password_hashed, display_name, email, AggregateVersion(0))
-        user._append_message(UserCreatedEvent(user.username, user.display_name, user._email))
+        user = cls(username=username, password_hashed=password_hashed, display_name=display_name, email=email,
+                   _aggregate_version=AggregateVersion(0))
+        user._append_message(UserCreatedEvent(username, display_name, email))
         return user
