e9cbe3b9d43ccddd39ab9cb227297ab5718c40d1
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 14:38:10 +0700

    test(chamber.data.model): add unit test test_should_raise_AttributeError_when_instantiate_with_unknown_attribute()
    Refs: #9

--- identity/lib/tests/chamber/test_data/test_model.py
+++ identity/lib/tests/chamber/test_data/test_model.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from chamber.aggregate import Field
 from chamber.data.model import DataModel
 
@@ -8,3 +10,11 @@ def test_should_instantiate_model_with_fields():
         my_string: str = Field()
 
     MyModel(my_number=123, my_string="Copy Paste Engineer")
+
+
+def test_should_raise_AttributeError_when_instantiate_with_unknown_attribute():
+    class MyModel(DataModel):
+        pass
+
+    with raises(AttributeError):
+        MyModel(something=1234)
