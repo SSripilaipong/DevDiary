d367db1e38b3d5b4cc3a2a4cbb69e7eaecf0c6ae
SSripilaipong <santhapon.s@siametrics.com>
2022-03-25 19:35:30 +0700

    refactor(identity.backend): use Message API for RegistrationEmailNeededToBeConfirmedEvent()
    Refs: #1

--- identity/backend/src/domain/identity/registration/event/confirmation_needed.py
+++ identity/backend/src/domain/identity/registration/event/confirmation_needed.py
@@ -1,31 +1,11 @@
-from typing import Dict
-
+from chamber.data.field import Field
 from chamber.message import Message
 from domain.identity.value_object.email import Email
 
 
 class RegistrationEmailNeededToBeConfirmedEvent(Message):
+    email: Email = Field(getter=True)
+    confirmation_code: str = Field("confirmationCode", getter=True)
+
     def __init__(self, email: Email, confirmation_code: str):
-        self._email = email
-        self._confirmation_code = confirmation_code
-
-    @property
-    def email(self) -> Email:
-        return self._email
-
-    @property
-    def confirmation_code(self) -> str:
-        return self._confirmation_code
-
-    def _body_to_dict(self) -> Dict:
-        return {
-            "email": self._email.str(),
-            "confirmation_code": self._confirmation_code,
-        }
-
-    @classmethod
-    def _body_from_dict(cls, obj: Dict) -> 'RegistrationEmailNeededToBeConfirmedEvent':
-        return cls(
-            email=Email.as_is(obj.get('email')),
-            confirmation_code=obj.get('confirmation_code'),
-        )
+        super().__init__(email=email, confirmation_code=confirmation_code)
