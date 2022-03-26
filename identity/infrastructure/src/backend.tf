fbb233a8933273b8373af61393bc7db9c9feafef
SSripilaipong <santhapon.s@siametrics.com>
2022-03-26 19:32:10 +0700

    fix(identity.infrastructure): fix parameter type error
    Refs: #12

--- identity/infrastructure/src/backend.tf
+++ identity/infrastructure/src/backend.tf
@@ -92,7 +92,7 @@ resource "aws_lambda_event_source_mapping" "relay" {
   event_source_arn  = aws_dynamodb_table.db.stream_arn
   function_name     = aws_lambda_function.backend.arn
   starting_position = "LATEST"
-  function_response_types = "ReportBatchItemFailures"
+  function_response_types = ["ReportBatchItemFailures"]
   batch_size = 10
   maximum_retry_attempts = 1  # experimental
 }
