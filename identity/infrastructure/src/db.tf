45f14f7415e5bf1e4792e57e234c75cd8f50865d
SSripilaipong <santhapon.s@siametrics.com>
2022-03-26 17:46:10 +0700

    build(identity.infrastructure): add dynamodb with policy
    Refs: #11

---
+++ identity/infrastructure/src/db.tf
@@ -0,0 +1,60 @@
+resource "aws_dynamodb_table" "db" {
+  name           = "${var.GLOBAL_PREFIX}-identity"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "_Partition"
+  range_key       = "_SortKey"
+
+  attribute {
+    name = "_Partition"
+    type = "S"
+  }
+
+  attribute {
+    name = "_SortKey"
+    type = "S"
+  }
+
+}
+
+resource "aws_iam_role_policy" "db-policy" {
+  role = aws_iam_role.backend-exec.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "ListAndDescribe"
+        Effect = "Allow"
+        Action = [
+          "dynamodb:List*",
+          "dynamodb:DescribeReservedCapacity*",
+          "dynamodb:DescribeLimits",
+          "dynamodb:DescribeTimeToLive",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid = "SpecificTable"
+        Effect = "Allow"
+        Action = [
+          "dynamodb:BatchGet*",
+          "dynamodb:DescribeStream",
+          "dynamodb:DescribeTable",
+          "dynamodb:Get*",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:BatchWrite*",
+          "dynamodb:CreateTable",
+          "dynamodb:Delete*",
+          "dynamodb:Update*",
+          "dynamodb:PutItem",
+        ]
+        Resource = aws_dynamodb_table.db.arn
+      },
+    ]
+  })
+
+  depends_on = [
+    aws_dynamodb_table.db,
+  ]
+}
