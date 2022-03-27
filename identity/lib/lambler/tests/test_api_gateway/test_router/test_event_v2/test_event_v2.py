8b5ec3aba2abce6e84351c6289b75b6aa89d9d38
SSripilaipong <santhapon.s@siametrics.com>
2022-03-27 17:04:52 +0700

    test(lambler.api_gateway): add unit test test_should_select_endpoints_with_same_method_by_path()
    Refs: #14

--- identity/lib/lambler/tests/test_api_gateway/test_router/test_event_v2/test_event_v2.py
+++ identity/lib/lambler/tests/test_api_gateway/test_router/test_event_v2/test_event_v2.py
@@ -66,3 +66,22 @@ def test_should_select_endpoints_with_same_path_by_method():
 
     router.match(simple_get_event("/default/something"), ...).handle()
     assert not getattr(post_something, "is_called", False) and getattr(get_something, "is_called", False)
+
+
+def test_should_select_endpoints_with_same_method_by_path():
+    router = APIGatewayRouter()
+
+    @router.get("/hello/one")
+    def hello_one():
+        hello_one.is_called = True
+
+    @router.get("/hello/two")
+    def hello_two():
+        hello_two.is_called = True
+
+    router.match(simple_post_event("/hello/one"), ...).handle()
+    assert getattr(hello_one, "is_called", False) and not getattr(hello_two, "is_called", False)
+    hello_one.is_called = False
+
+    router.match(simple_get_event("/hello/two"), ...).handle()
+    assert not getattr(hello_one, "is_called", False) and getattr(hello_two, "is_called", False)
