da3cf36bb185d92b49b8df064d76c13072ddc583
SSripilaipong <santhapon.s@siametrics.com>
2022-03-28 00:24:26 +0700

    feat(lambler.api_gateway): handle event v2 in router for unit test test_should_read_event_v2_when_specify_event_version_in_router()
    Refs: #16

---
+++ identity/lib/lambler/lambler/api_gateway/aws/version.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class AWSEventVersion(str, Enum):
+    V2 = "V2"
--- identity/lib/lambler/lambler/api_gateway/router.py
+++ identity/lib/lambler/lambler/api_gateway/router.py
@@ -2,6 +2,8 @@ from typing import Dict, Any, Optional, List
 import bisect
 import pydantic
 
+from lambler.api_gateway.aws.event_v2 import AWSAPIGatewayEventV2
+from lambler.api_gateway.aws.version import AWSEventVersion
 from lambler.api_gateway.endpoint import Endpoint
 from lambler.api_gateway.event import APIGatewayEvent
 from lambler.api_gateway.method import RequestMethodEnum
@@ -31,13 +33,14 @@ class EndpointSortWrapper:
 
 class APIGatewayRouter(HandlerMatcher):
     def __init__(self, *, event_version=None):
+        self._event_version = _validate_event_version(event_version)
         self._endpoints: List[EndpointSortWrapper] = []
 
     def match(self, event: Dict, context: Any) -> Optional[Handler]:
-        try:
-            api_event = APIGatewayEvent(**event)
-        except pydantic.ValidationError:
-            return None
+        api_event = self._validate_event(event)
+        return self._match_event_with_endpoints(api_event)
+
+    def _match_event_with_endpoints(self, api_event):
         for wrapper in self._endpoints:
             endpoint = wrapper.endpoint
             if endpoint.match(api_event):
@@ -59,6 +62,16 @@ class APIGatewayRouter(HandlerMatcher):
     def _append_endpoint(self, endpoint: Endpoint):
         bisect.insort_right(self._endpoints, EndpointSortWrapper(endpoint))
 
+    def _validate_event(self, event: Dict) -> Optional[APIGatewayEvent]:
+        try:
+            if self._event_version is None:
+                return APIGatewayEvent(**event)
+            if self._event_version == AWSEventVersion.V2:
+                return AWSAPIGatewayEventV2(**event).normalize()
+        except pydantic.ValidationError:
+            return None
+        raise NotImplementedError()
+
 
 class APIGatewayEventHandler(Handler):
     def __init__(self, endpoint: Endpoint, event: APIGatewayEvent):
@@ -67,3 +80,10 @@ class APIGatewayEventHandler(Handler):
 
     def handle(self) -> Any:
         self._endpoint.handle(self._event)
+
+
+def _validate_event_version(version: str) -> Optional[AWSEventVersion]:
+    if version is None:
+        return None
+
+    return AWSEventVersion[version.upper()]
