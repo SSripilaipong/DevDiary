11f021f9336990404a3872fc895ebd87605ac4a0
SSripilaipong <santhapon.s@siametrics.com>
2022-03-29 19:02:50 +0700

    feat(lambler): support return type LamblerResponse for unit test test_should_convert_LamblerResponse_to_dict()
    Refs: #23

--- identity/lib/lambler/lambler/lambler.py
+++ identity/lib/lambler/lambler/lambler.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Any
 
 from lambler.base.handler import PatternMatcher
+from lambler.base.response import LamblerResponse
 
 
 class Lambler:
@@ -8,14 +9,19 @@ class Lambler:
         self.__patterns: List[PatternMatcher] = []
 
     def __call__(self, event: Dict, context: Any) -> Any:
-        for pattern in self.__patterns:
-            matched = pattern.match(event, context)
-            if matched is not None:
-                break
-        else:
-            raise NotImplementedError()
+        handler = self.__get_matched_handler(event, context)
+        response = handler.handle()
 
-        return matched.handle()
+        if isinstance(response, LamblerResponse):
+            return response.to_dict()
+        return response
 
     def include_pattern(self, pattern: PatternMatcher):
         self.__patterns.append(pattern)
+
+    def __get_matched_handler(self, event: Dict, context: Any):
+        for pattern in self.__patterns:
+            matched = pattern.match(event, context)
+            if matched is not None:
+                return matched
+        raise NotImplementedError()
