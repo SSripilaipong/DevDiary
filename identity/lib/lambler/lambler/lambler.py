aef5f333ad4f6c469edce90e0bb7ab1c8d10aad2
SSripilaipong <santhapon.s@siametrics.com>
2022-03-29 18:44:28 +0700

    test(lambler): add unit test test_should_return_from_matched_handler()
    Refs: #23

--- identity/lib/lambler/lambler/lambler.py
+++ identity/lib/lambler/lambler/lambler.py
@@ -1,3 +1,9 @@
+from lambler.base.handler import PatternMatcher
+
+
 class Lambler:
     def __call__(self, *args, **kwargs):
         return None
+
+    def include_pattern(self, pattern: PatternMatcher):
+        pass
--- identity/lib/lambler/tests/test_lambler/test_router.py
+++ identity/lib/lambler/tests/test_lambler/test_router.py
@@ -1,5 +1,24 @@
+from typing import Dict, Any
+
 from lambler import Lambler
+from lambler.base.handler import PatternMatcher, Handler
 
 
 def test_should_return_None_when_no_matchers():
     assert Lambler()() is None
+
+
+def test_should_return_from_matched_handler():
+    class MyHandler(Handler):
+        def handle(self) -> Any:
+            return "OK!"
+
+    class MyPattern(PatternMatcher):
+        def match(self, event: Dict, context: Any) -> MyHandler:
+            return MyHandler()
+
+    lambler = Lambler()
+    lambler.include_pattern(MyPattern())
+
+    assert lambler({}, ...) == "OK!"
+
