988900518b9c0347431cf4bfbb853578ff1c47cc
SSripilaipong <santhapon.s@siametrics.com>
2022-03-29 14:03:28 +0700

    test(lambler.api_gateway): refactor test for passing json body
    Refs: #19

--- identity/lib/lambler/tests/test_api_gateway/test_post_endpoint.py
+++ identity/lib/lambler/tests/test_api_gateway/test_post_endpoint.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Dict
+from typing import Dict, Type, Callable
 
 from pytest import raises
 
@@ -10,27 +10,19 @@ from .event_factory import simple_post_event
 
 
 def test_should_pass_json_body_as_dict():
-    router = APIGatewayRouter()
-
-    @router.post("/do/something")
-    def do_something(my_body: dict = JSONBody()):
-        do_something.data = my_body["data"]
-
-    event = simple_post_event("/do/something", {"data": "Hello World"}, headers={"content-type": "application/json"})
-    router.match(event, ...).handle()
-    assert getattr(do_something, "data", "") == "Hello World"
+    _test_passing_json_body(dict, lambda body, name: body[name])
 
 
 def test_should_pass_json_body_as_Dict():
-    router = APIGatewayRouter()
+    _test_passing_json_body(Dict, lambda body, name: body[name])
 
-    @router.post("/do/something")
-    def do_something(my_body: Dict = JSONBody()):
-        do_something.data = my_body["data"]
 
-    event = simple_post_event("/do/something", {"data": "Hello World"}, headers={"content-type": "application/json"})
-    router.match(event, ...).handle()
-    assert getattr(do_something, "data", "") == "Hello World"
+def test_should_pass_pydantic_BaseModel():
+    class MyModel(BaseModel):
+        my_name: str
+        my_number: int
+
+    _test_passing_json_body(MyModel, lambda body, name: getattr(body, name))
 
 
 def test_should_raise_InvalidParameterError_when_request_JSONBody_without_content_type_json():
@@ -43,23 +35,6 @@ def test_should_raise_InvalidParameterError_when_request_JSONBody_without_conten
     event = simple_post_event("/do/something", {"data": "Hello World"})
     with raises(InvalidParameterError):
         router.match(event, ...).handle()
-
-
-def test_should_pass_pydantic_BaseModel():
-    router = APIGatewayRouter()
-
-    class MyModel(BaseModel):
-        my_name: str
-        my_number: int
-
-    @router.post("/do/something")
-    def do_something(input_data: MyModel = JSONBody()):
-        do_something.data = {"my_name": input_data.my_name, "my_number": input_data.my_number}
-
-    event = simple_post_event("/do/something", {"my_name": "Hello", "my_number": 123},
-                              headers={"content-type": "application/json"})
-    router.match(event, ...).handle()
-    assert getattr(do_something, "data", "") == {"my_name": "Hello", "my_number": 123}
 
 
 def test_should_raise_InvalidParameterError_when_request_JSONBody_failed_to_convert_to_json():
@@ -103,3 +78,15 @@ def test_should_raise_InvalidParameterError_when_request_JSONBody_not_fit_to_mod
         router.match(event, ...).handle()
 
 
+def _test_passing_json_body(model: Type, get_attr: Callable):
+    router = APIGatewayRouter()
+
+    @router.post("/do/something")
+    def do_something(input_data: model = JSONBody()):
+        do_something.data = {"my_name": get_attr(input_data, "my_name"),
+                             "my_number": get_attr(input_data, "my_number")}
+
+    event = simple_post_event("/do/something", {"my_name": "Hello", "my_number": 123},
+                              headers={"content-type": "application/json"})
+    router.match(event, ...).handle()
+    assert getattr(do_something, "data", "") == {"my_name": "Hello", "my_number": 123}
