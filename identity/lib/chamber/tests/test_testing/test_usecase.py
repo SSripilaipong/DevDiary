28e7e1497148430c7e4d8e6c74fe7237bd655167
SSripilaipong <santhapon.s@siametrics.com>
2022-03-30 23:49:52 +0700

    test(chamber.testing): add completed unit test test_should_be_able_to_run_real_usecase_outside_mock_area()
    Refs: #27

--- identity/lib/chamber/tests/test_testing/test_usecase.py
+++ identity/lib/chamber/tests/test_testing/test_usecase.py
@@ -26,3 +26,19 @@ def test_should_run_the_test_function():
 
     not_gonna_do_it()
     assert getattr(not_gonna_do_it, "done", False)
+
+
+def test_should_be_able_to_run_real_usecase_outside_mock_area():
+    @usecase
+    def do_something():
+        do_something.done = True
+
+    @mock_usecase(do_something)
+    def not_gonna_do_it():
+        do_something()
+
+    not_gonna_do_it()
+    assert not getattr(do_something, "done", False)
+
+    do_something()
+    assert getattr(do_something, "done", False)
