19f72907d7c83496d9d1b32c0abb81b939ccc11e
SSripilaipong <santhapon.s@siametrics.com>
2022-03-30 23:34:12 +0700

    feat(chamber.testing): implement @mock_usecase for unit test test_should_not_call_real_usecase_when_mocking()
    Refs: #27

--- identity/lib/chamber/chamber/testing/usecase.py
+++ identity/lib/chamber/chamber/testing/usecase.py
@@ -1,8 +1,15 @@
 from typing import TypeVar, Callable
+from functools import wraps
 
 
 T = TypeVar("T", bound=Callable)
+U = TypeVar("U", bound=Callable)
 
 
-def mock_usecase(func: T) -> T:
-    pass
+def mock_usecase(usecase_function: U):
+    def decorator(func: T) -> T:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            pass
+        return wrapper
+    return decorator
