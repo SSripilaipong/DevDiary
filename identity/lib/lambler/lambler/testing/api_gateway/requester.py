d15864298ef0907c9480f482c46269e5b38f556c
SSripilaipong <santhapon.s@siametrics.com>
2022-03-30 15:02:54 +0700

    feat(lambler.testing): add parameter body: Dict in HTTPRequester.post() for unit test test_should_make_post_request_with_json_body()
    Refs: #25

--- identity/lib/lambler/lambler/testing/api_gateway/requester.py
+++ identity/lib/lambler/lambler/testing/api_gateway/requester.py
@@ -1,3 +1,7 @@
+from typing import Dict
+
+import json
+
 from lambler import Lambler
 from lambler.api_gateway.method import RequestMethodEnum
 from lambler.api_gateway.response import APIGatewayResponse
@@ -10,15 +14,25 @@ class HTTPRequester:
     def get(self, path: str) -> APIGatewayResponse:
         return self.__call_lambler(path, RequestMethodEnum.GET)
 
-    def post(self, path: str, body: dict = None) -> APIGatewayResponse:
-        return self.__call_lambler(path, RequestMethodEnum.POST)
+    def post(self, path: str, body: Dict = None) -> APIGatewayResponse:
+        if body is None:
+            body = ""
+            headers = {}
+        elif isinstance(body, dict):
+            body = json.dumps(body)
+            headers = {"content-type": "application/json"}
+        else:
+            raise NotImplementedError()
 
-    def __call_lambler(self, path: str, method: RequestMethodEnum) -> APIGatewayResponse:
+        return self.__call_lambler(path, RequestMethodEnum.POST, body=body, headers=headers)
+
+    def __call_lambler(self, path: str, method: RequestMethodEnum, body: str = "",
+                       headers: Dict = None) -> APIGatewayResponse:
         event = {
             "path": path,
             "method": method,
             "query_string_parameters": {},
-            "headers": {},
-            "body": "",
+            "headers": headers or {},
+            "body": body,
         }
         return self._lambler.call_raw_response(event, ...)
