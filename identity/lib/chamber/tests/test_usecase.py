ab7872dcc0a51a54868810ef5e2b785d62cba298
SSripilaipong <santhapon.s@siametrics.com>
2022-03-31 13:19:54 +0700

    test(chamber.testing): add unit test test_should_raise_TypeError_when_return_type_is_not_annotated()
    Refs: #27

--- identity/lib/chamber/tests/test_usecase.py
+++ identity/lib/chamber/tests/test_usecase.py
@@ -1,10 +1,19 @@
+from pytest import raises
 from chamber import usecase
 
 
 def test_should_run_usecase_function():
     @usecase
-    def do_something():
+    def do_something() -> int:
         do_something.done = True
+        return 0
 
     do_something()
     assert getattr(do_something, "done", False)
+
+
+def test_should_raise_TypeError_when_return_type_is_not_annotated():
+    with raises(TypeError):
+        @usecase
+        def do_something():
+            return 0
