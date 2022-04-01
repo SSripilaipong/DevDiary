dd1dd3e450e4933a27a0927bbe58d9d0619d9c0f
SSripilaipong <santhapon.s@siametrics.com>
2022-04-01 16:30:05 +0700

    test(lambler.api_gateway): add unit test test_should_respond_internal_server_error_when_endpoint_raises_unexpected_exception()
    Refs: #30

--- identity/lib/lambler/tests/test_api_gateway/test_errors.py
+++ identity/lib/lambler/tests/test_api_gateway/test_errors.py
@@ -19,3 +19,17 @@ def test_should_response_unprocessable_entity_when_json_body_required_but_none_g
     requester = HTTPRequester(lambler, event_version=AWSEventVersion.V2)
     response = requester.post("/")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+def test_should_respond_internal_server_error_when_endpoint_raises_unexpected_exception():
+    lambler = Lambler()
+    router = APIGatewayRouter(event_version=AWSEventVersion.V2)
+
+    @router.get("/")
+    def get():
+        raise ZeroDivisionError()
+
+    lambler.include_pattern(router)
+    requester = HTTPRequester(lambler, event_version=AWSEventVersion.V2)
+    response = requester.get("/")
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
