e3fd7541868f0e7b41235f3f8697ad58e29a9666
SSripilaipong <santhapon.s@siametrics.com>
2022-04-01 15:40:14 +0700

    test(lambler.api_gateway): add unit test test_should_response_unprocessable_entity_when_json_body_required_but_none_given()
    Refs: #30

---
+++ identity/lib/lambler/tests/test_api_gateway/test_errors.py
@@ -0,0 +1,19 @@
+from lambler import Lambler
+from lambler.api_gateway.aws.version import AWSEventVersion
+from lambler.api_gateway.endpoint.marker import JSONBody
+from lambler.api_gateway.router import APIGatewayRouter
+from lambler.testing.api_gateway.requester import HTTPRequester
+
+
+def test_should_response_unprocessable_entity_when_json_body_required_but_none_given():
+    lambler = Lambler()
+    router = APIGatewayRouter(event_version=AWSEventVersion.V2)
+
+    @router.post("/")
+    def post(data: dict = JSONBody()):
+        return ""
+
+    lambler.include_pattern(router)
+    requester = HTTPRequester(lambler, event_version=AWSEventVersion.V2)
+    response = requester.post("/")
+    assert response.status_code == 422
