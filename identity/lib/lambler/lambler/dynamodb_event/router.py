0e17b85ebf13da3934fc3ccd795fa31cabe8a054
SSripilaipong <SHSnail@mail.com>
2022-04-05 13:39:29 +0700

    refactor(lambler.dynamodb_event): remove unused code in DynamodbEventRouter
    Refs: #35

--- identity/lib/lambler/lambler/dynamodb_event/router.py
+++ identity/lib/lambler/lambler/dynamodb_event/router.py
@@ -4,19 +4,9 @@ from typing import Callable, List, Optional, Iterator, TYPE_CHECKING, Any, Dict
 from lambler.base.event import LamblerEvent
 from lambler.base.handler import Handler, PatternMatcher
 from lambler.base.router import Router
-from lambler.dynamodb_event.endpoint import DynamodbEventEndpoint
+from lambler.dynamodb_event.endpoint import DynamodbEventEndpoint, DynamodbEventHandler
 from lambler.dynamodb_event.event import DynamodbEvent
-from lambler.dynamodb_event.response import DynamodbEventResponse
 from lambler.dynamodb_event.type import DynamodbEventType
-
-
-class DynamodbEventHandler(Handler):
-    def __init__(self, endpoint: DynamodbEventEndpoint, event: DynamodbEvent):
-        self._endpoint = endpoint
-        self._event = event
-
-    def handle(self) -> DynamodbEventResponse:
-        return self._endpoint.process(self._event)
 
 
 class DynamodbEventRouter(Router):
@@ -24,10 +14,7 @@ class DynamodbEventRouter(Router):
         self._endpoints: List[DynamodbEventEndpoint] = []
 
     def subscribe_insert(self, handle: Callable):
-        self._append_endpoint(DynamodbEventEndpoint(method=DynamodbEventType.INSERT, handle=handle))
-
-    def _append_endpoint(self, endpoint):
-        self._endpoints.append(endpoint)
+        self._endpoints.append(DynamodbEventEndpoint(method=DynamodbEventType.INSERT, handle=handle))
 
     def _validate_event(self, event) -> Optional[DynamodbEvent]:
         try:
@@ -37,9 +24,6 @@ class DynamodbEventRouter(Router):
 
     def _iterate_patterns(self) -> Iterator[PatternMatcher]:
         yield from self._endpoints
-
-    def _make_handler(self, endpoint: DynamodbEventEndpoint, event: DynamodbEvent) -> Handler:
-        return DynamodbEventHandler(endpoint, event)
 
     def _on_no_pattern_matched(self, event: LamblerEvent) -> Optional[Handler]:
         pass
