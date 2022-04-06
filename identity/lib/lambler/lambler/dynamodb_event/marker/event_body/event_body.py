285af85fa200dfbcef5f13553a586fae757b301b
SSripilaipong <SHSnail@mail.com>
2022-04-06 17:56:11 +0700

    refactor(lambler.dynamodb_event): use parser_factory for EventBody Marker
    Refs: #32

--- identity/lib/lambler/lambler/dynamodb_event/marker/event_body/event_body.py
+++ identity/lib/lambler/lambler/dynamodb_event/marker/event_body/event_body.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
-from typing import Dict, Any, Type, Optional, Callable
+from typing import Any, Type, Optional
 
+from lambler.base.data.parser import parser_factory, Parser
+from lambler.base.data.parser.exception import DataParsingError
 from lambler.base.function.input import FunctionInputSourceCollection
 from lambler.base.function.marker import Marker
 from lambler.dynamodb_event.input.new_image import DynamodbEventNewImageInputSource
@@ -8,19 +9,20 @@ from lambler.dynamodb_event.input.new_image import DynamodbEventNewImageInputSou
 
 class EventBody(Marker):
     def __init__(self):
-        self._cast: Optional[Callable] = None
+        self._parser: Optional[Parser] = None
 
     def register_type(self, type_: Type):
-        if type_ is dict or type_ is Dict:
-            self._cast = None
-        elif isinstance(type_, type) and issubclass(type_, BaseModel):
-            self._cast = lambda d: type_(**d)
-        else:
+        try:
+            self._parser = parser_factory.for_type(type_)
+        except TypeError:
             raise NotImplementedError()
 
     def extract_param(self, data: FunctionInputSourceCollection) -> Any:
         new_image = data.of(DynamodbEventNewImageInputSource)
         data = new_image.to_dict()
-        if self._cast is None:
-            return data
-        return self._cast(data)
+        if self._parser is None:
+            raise NotImplementedError()
+        try:
+            return self._parser.parse(data)
+        except DataParsingError:
+            raise NotImplementedError()
