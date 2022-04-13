6c42b08582f2d5e336d66bd31675a156a0c81034
SSripilaipong <SHSnail@mail.com>
2022-04-13 18:29:05 +0700

    fix(lambler.dynamodb_event): make deserialize() returns in bytes instead of Binary
    Refs: #46

--- identity/lib/lambler/lambler/dynamodb_event/input/new_image.py
+++ identity/lib/lambler/lambler/dynamodb_event/input/new_image.py
@@ -8,7 +8,7 @@ from lambler.dynamodb_event.data.event import DynamodbEvent
 class DynamodbEventNewImageInputSource(FunctionInputSource):
     def __init__(self, data: Dict):
         self._data = data
-        self._deserializer = TypeDeserializer()
+        self._deserializer = MyTypeDeserializer()
 
     @classmethod
     def from_event(cls, event: DynamodbEvent) -> 'DynamodbEventNewImageInputSource':
@@ -16,3 +16,11 @@ class DynamodbEventNewImageInputSource(FunctionInputSource):
 
     def to_dict(self) -> Dict:
         return {key: self._deserializer.deserialize(value) for key, value in self._data.items()}
+
+
+class MyTypeDeserializer(TypeDeserializer):
+    def _deserialize_b(self, value: str) -> bytes:
+        if isinstance(value, str):
+            value = value.encode("ascii")
+        assert isinstance(value, bytes)
+        return value
