b6fe6df887835172840099e168e6631c570fd349
SSripilaipong <SHSnail@mail.com>
2022-04-18 14:26:59 +0700

    test(lambler.sns.message): add unit test test_should_not_call_function_when_topic_not_matched()
    Refs: #47

--- identity/lib/lambler/tests/test_sns_message/test_processor.py
+++ identity/lib/lambler/tests/test_sns_message/test_processor.py
@@ -11,3 +11,14 @@ def test_should_call_function():
 
     processor.match(simple_notification_event("my-topic"), ...).handle()
     assert getattr(on_message, "is_called", False)
+
+
+def test_should_not_call_function_when_topic_not_matched():
+    processor = SNSMessageProcessor()
+
+    @processor.message("my-topic")
+    def on_message():
+        on_message.is_called = True
+
+    processor.match(simple_notification_event("not-my-topic"), ...).handle()
+    assert not getattr(on_message, "is_called", False)
