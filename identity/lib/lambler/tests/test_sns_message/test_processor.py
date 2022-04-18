3168e3cc31f95b8fa89f83461441b27af3ae898e
SSripilaipong <SHSnail@mail.com>
2022-04-18 18:04:00 +0700

    test(lambler.sns.message): add unit test test_should_pass_payload_as_chamber_data_model()
    Refs: #47

--- identity/lib/lambler/tests/test_sns_message/test_processor.py
+++ identity/lib/lambler/tests/test_sns_message/test_processor.py
@@ -1,7 +1,9 @@
 from pytest import raises
 from pydantic import BaseModel
-from typing import Dict, Type, Any
+from typing import Dict, Type, TypeVar
 
+from chamber.data.field import Field
+from chamber.data.model import DataModel
 from lambler.base.data.parser.exception import DataParsingError
 from lambler.sns.message.marker import MessageBody
 from lambler.sns.message.processor import SNSMessageProcessor
@@ -31,15 +33,15 @@ def test_should_not_call_function_when_topic_not_matched():
 
 
 def test_should_pass_payload_as_str():
-    _test_passed_payload("Hello World", str, "Hello World")
+    assert _get_passed_payload("Hello World", str) == "Hello World"
 
 
 def test_should_pass_payload_as_Dict():
-    _test_passed_payload('{"data": "abc", "num": 1234}', Dict, {"data": "abc", "num": 1234})
+    assert _get_passed_payload('{"data": "abc", "num": 1234}', Dict) == {"data": "abc", "num": 1234}
 
 
 def test_should_pass_payload_as_dict():
-    _test_passed_payload('{"data": "abc", "num": 1234}', dict, {"data": "abc", "num": 1234})
+    assert _get_passed_payload('{"data": "abc", "num": 1234}', dict) == {"data": "abc", "num": 1234}
 
 
 def test_should_raise_DataParsingError_when_cannot_parse_to_Dict():
@@ -55,7 +57,7 @@ def test_should_pass_payload_as_pydantic_model():
         data: str
         num: int
 
-    _test_passed_payload('{"data": "abc", "num": 1234}', MyModel, MyModel(data="abc", num=1234))
+    assert _get_passed_payload('{"data": "abc", "num": 1234}', MyModel) == MyModel(data="abc", num=1234)
 
 
 def test_should_raise_DataParsingError_when_having_non_json_value_for_pydantic_model():
@@ -74,6 +76,15 @@ def test_should_raise_DataParsingError_when_having_invalid_structure_for_pydanti
     _test_raise_DataParsingError('{"Copy": "Paste", "data": "Something"}', MyModel)
 
 
+def test_should_pass_payload_as_chamber_data_model():
+    class MyModel(DataModel):
+        data: str = Field(getter=True)
+        num: int = Field(getter=True)
+
+    payload = _get_passed_payload('{"data": "abc", "num": 1234}', MyModel)
+    assert payload.data == "abc" and payload.num == 1234
+
+
 def _test_raise_DataParsingError(message: str, type_: Type):
     processor = SNSMessageProcessor()
 
@@ -85,7 +96,10 @@ def _test_raise_DataParsingError(message: str, type_: Type):
         processor.match(simple_notification_event("my-topic", message=message), ...).handle()
 
 
-def _test_passed_payload(message: str, type_: Type, expected_payload: Any):
+T = TypeVar("T")
+
+
+def _get_passed_payload(message: str, type_: Type[T]) -> T:
     processor = SNSMessageProcessor()
 
     @processor.message("my-topic")
@@ -93,4 +107,4 @@ def _test_passed_payload(message: str, type_: Type, expected_payload: Any):
         on_message.payload = payload_
 
     processor.match(simple_notification_event("my-topic", message=message), ...).handle()
-    assert getattr(on_message, "payload", None) == expected_payload
+    return getattr(on_message, "payload", None)
