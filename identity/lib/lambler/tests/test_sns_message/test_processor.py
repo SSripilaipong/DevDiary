c0f9025e67c91ba54c3858f3eff19839584dfc8d
SSripilaipong <SHSnail@mail.com>
2022-04-18 17:43:52 +0700

    test(lambler.sns.message): add unit test test_should_raise_DataParsingError_when_cannot_parse_to_dict()
    Refs: #47

--- identity/lib/lambler/tests/test_sns_message/test_processor.py
+++ identity/lib/lambler/tests/test_sns_message/test_processor.py
@@ -1,6 +1,8 @@
+from pytest import raises
 from pydantic import BaseModel
 from typing import Dict, Type, Any
 
+from lambler.base.data.parser.exception import DataParsingError
 from lambler.sns.message.marker import MessageBody
 from lambler.sns.message.processor import SNSMessageProcessor
 from tests.test_sns_message.event_factory import simple_notification_event
@@ -40,12 +42,27 @@ def test_should_pass_payload_as_dict():
     _test_passed_payload('{"data": "abc", "num": 1234}', dict, {"data": "abc", "num": 1234})
 
 
+def test_should_raise_DataParsingError_when_cannot_parse_to_dict():
+    _test_raise_DataParsingError('Cannot Parse', dict)
+
+
 def test_should_pass_payload_as_pydantic_model():
     class MyModel(BaseModel):
         data: str
         num: int
 
     _test_passed_payload('{"data": "abc", "num": 1234}', MyModel, MyModel(data="abc", num=1234))
+
+
+def _test_raise_DataParsingError(message: str, type_: Type):
+    processor = SNSMessageProcessor()
+
+    @processor.message("my-topic")
+    def on_message(payload_: type_ = MessageBody()):
+        on_message.payload = payload_
+
+    with raises(DataParsingError):
+        processor.match(simple_notification_event("my-topic", message=message), ...).handle()
 
 
 def _test_passed_payload(message: str, type_: Type, expected_payload: Any):
